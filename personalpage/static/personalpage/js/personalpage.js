c02983ae3c3d3bb1370366cc6aef43f44cd46900
Demmenty <Demmenty@gmail.com>
2022-09-10 00:30:15 +0300

    сделано перетаскивание комментов

--- personalpage/static/personalpage/js/personalpage.js
+++ personalpage/static/personalpage/js/personalpage.js
@@ -3,7 +3,6 @@ commentForm = document.getElementById("comment1");
 
 // показать\скрыть коммент соответствующей даты
 function changeComment(event) {
-
     // открытая форма скрывается
     commentForm.classList.add("hidden_element");
     // соответствующая ей кнопка сереет
@@ -21,13 +20,15 @@ function changeComment(event) {
     commentForm = document.getElementById("comment" + commentNum);
     // показываем эту форму
     commentForm.classList.remove("hidden_element");
+
+    dragElement(commentForm);
 }
 
 commentButtons.forEach ( btn => {
     btn.addEventListener('click', changeComment, false); 
 })
 
-// закрывание окошка коммента
+// закрывание окошка коммента на крестик
 function closeComment(event) {
     // находим номер нажатой кнопки
     commentNum = event.target.getAttribute("id")[17];
@@ -41,4 +42,48 @@ function closeComment(event) {
     commentBtn = document.getElementById("comment_btn" + commentNum);
     // и закрашиваем
     commentBtn.style.mixBlendMode = "luminosity";
+}
+
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    console.log("yep")
+    // если присутствует, заголовок - это место, откуда вы перемещаете DIV:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    console.log("no")
+    // в противном случае переместите DIV из любого места внутри DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // получить положение курсора мыши при запуске:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // вызов функции при каждом перемещении курсора:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // вычислить новую позицию курсора:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // установите новое положение элемента:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // остановка перемещения при отпускании кнопки мыши:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
