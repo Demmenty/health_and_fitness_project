feb2fd545a41736b7b40bf20359181338311cbcc
Demmenty <Demmenty@gmail.com>
2022-09-12 12:49:12 +0300

    улучшен календарик недели

--- personalpage/static/personalpage/js/addmeasure.js
+++ personalpage/static/personalpage/js/addmeasure.js
@@ -1,25 +1,26 @@
-calendarTable = document.getElementById('week_calendar_table');
-// получение даты в календаре за последний день
-chosenDate = document.querySelectorAll('.select_date')[6];
+// все кнопки выбора дат за неделю
+choseDateBtns = document.querySelectorAll('.date_selection');
+// последний день в календаре недели
+chosenDate = choseDateBtns[6];
 // получение формы за последний день
 dateValue = chosenDate.getAttribute('value');
 chosenForm = document.getElementById(dateValue);
 
 // последняя дата сразу выбрана
-chosenDate.className = "selected_date";
+chosenDate.classList.add("selected_date");
 // и последняя форма отображена
 chosenForm.classList.remove("hidden_element");
 
 // функция изменения даты и формы на ту, что кликнута
 function ChangeDate(event) {
-    // старая дата обесцвечивается
-    chosenDate.className = "select_date";
+    // старая кнопка даты обесцвечивается
+    chosenDate.classList.remove("selected_date");
     // находится элемент, по кот кликнули
     newDate = event.target;
     // определение его значения (даты)
     newdateValue = newDate.getAttribute('value');
     // закрашивание элемента, на кот кликнули
-    newDate.className = "selected_date";
+    newDate.classList.add("selected_date");
     // теперь новый элемент назначается выбранным
     chosenDate = newDate
     // старая форма скрывается
@@ -32,5 +33,7 @@ function ChangeDate(event) {
     chosenForm = newForm;
 }
 
-// добавление функции реагирующей на клик к таблице
-calendarTable.addEventListener('click', ChangeDate, false);
+// добавление функции реагирующей на клик к значкам дат
+choseDateBtns.forEach ( choseDateBtn => {
+    choseDateBtn.addEventListener('click', ChangeDate, false);
+})
