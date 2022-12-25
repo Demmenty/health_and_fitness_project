6802416c166f29918bc069113479f78589850f5d
Demmenty <Demmenty@gmail.com>
2022-12-25 14:31:50 +0400

    what_section styled for md >

--- controlpage/models.py
+++ controlpage/models.py
@@ -30,7 +30,7 @@ class ConsultationSignup(models.Model):
     name = models.CharField('Имя', max_length=100, help_text='Как к вам обращаться?')
     age = models.CharField('Возраст', max_length=100, blank=True, help_text='Сколько вам лет?')
     location = models.CharField('Место жительства', max_length=100, blank=True, help_text='Расскажите, откуда вы?')
-    email = models.EmailField('Почта', max_length=100, blank=True, help_text='Обещаем не использовать в коварных целях!')
+    email = models.EmailField('Почта', max_length=100, blank=True, help_text='Обещаем не использовать в коварных планах!')
     contacts = models.CharField('Контакты', max_length=255, help_text='Оставьте ссылку на предпочитаемый способ связи')
     is_read = models.BooleanField('Заявка прочитана', default=False)
     expert_note = models.TextField('Заметка', default='', blank=True)
