40113fd091ce3c81348fa6c55cf3c09bee0b7722
Demmenty <Demmenty@gmail.com>
2023-01-26 14:38:56 +0400

    переименования проекта

--- health_and_fitness/urls.py
+++ health_and_fitness/urls.py
@@ -1,4 +1,4 @@
-"""fitnessparabola URL Configuration
+"""health_and_fitness URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
