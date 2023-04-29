e83461336214f62076b0ff8ef9748b06e27409a3
Demmenty <Demmenty@gmail.com>
2023-04-29 18:20:20 +0400

    + миксин ограничения доступа при ajax запросах

---
+++ common/mixins.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
+
+
+class ClientSelfOrExpertAllowedAJAXMixin(AccessMixin):
+    """Запрос разрешен от лица самого клиента либо эксперта"""
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("Необходима авторизация")
+
+        if request.method == "GET":
+            client_id = request.GET.get("client")
+        elif request.method == "POST":
+            client_id = request.POST.get("client")
+
+        if not client_id:
+            return HttpResponseBadRequest("Необходим client_id")
+
+        if not request.user.is_expert:
+            if request.user.id != int(client_id):
+                return HttpResponseForbidden("У вас нет прав на это действие")
+
+        return super().dispatch(request, *args, **kwargs)
