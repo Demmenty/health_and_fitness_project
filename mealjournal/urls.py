8e1e377870849f05c851bf39ae644295881332ab
Demmenty <Demmenty@gmail.com>
2023-05-28 23:08:00 +0400

    удаление более ненужных файлов сводок питания

--- mealjournal/urls.py
+++ mealjournal/urls.py
@@ -4,8 +4,6 @@ from mealjournal import views, ajax
 
 urlpatterns = [
     path("", views.mealjournal_page, name="mealjournal_page"),
-    path("foodbydate/", views.foodbydate_page, name="foodbydate_page"),
-    path("foodbymonth/", views.foodbymonth_page, name="foodbymonth_page"),
     path("ajax/get_briefbydate", ajax.get_briefbydate, name="get_briefbydate"),
     path("ajax/get_briefbymonth", ajax.get_briefbymonth, name="get_briefbymonth"),
 ]
