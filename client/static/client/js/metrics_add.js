dc5ac49c1c702401388d61ce4a0fc40edcb76dfa
Demmenty <Demmenty@gmail.com>
2023-11-15 13:53:58 +0400

    metric day select impovement

--- client/static/client/js/metrics_add.js
+++ client/static/client/js/metrics_add.js
@@ -1,5 +1,8 @@
 const metricsForm = $("#metrics-form");
 const fsConnectStatus = $("#fs-connect-status");
+const dayInput = $("#day-input");
+const prevDayBtn = dayInput.find(".prev-arrow");
+const nextDayBtn = dayInput.find(".next-arrow");
 
 const nutritionFields = {
     "calories": metricsForm.find(`#id_calories`),
@@ -12,6 +15,9 @@ const nutritionFields = {
 
 $(document).ready(function () {
     updateNutritionFromFS();
+
+    prevDayBtn.on("click", showPrevDay);
+    nextDayBtn.on("click", showNextDay);
 })
 
 // REQUESTS
@@ -26,6 +32,17 @@ async function getNutritionFromFSRequest() {
         type: "GET",
         url: fsConnectStatus.data("get-nutrition-url"),
     })
+}
+
+/**
+ * Redirects the user to a specific day by updating the URL with the provided day.
+ *
+ * @param {string} day - The day to redirect the user to.
+ */
+function redirectToDay(day) {
+    const queryString = new URLSearchParams({ date: day }).toString();
+    const url = window.location.href.split('?')[0] + '?' + queryString;
+    window.location.href = url;
 }
 
 // FUNCS
@@ -65,4 +82,40 @@ async function updateNutritionFromFS() {
         }
         field.addClass("bg-light").attr("readonly", true);
     }
+}
+
+/**
+ * Displays the metric form for the previous day.
+ */
+function showPrevDay() {
+    const day = new Date(dayInput.data("day"));
+    const requestDay = addDays(day, -1);
+
+    redirectToDay(requestDay);
+}
+
+/**
+ * Shows the metric form for the next day.
+ */
+function showNextDay() {
+    const day = new Date(dayInput.data("day"));
+    const requestDay = addDays(day, 1);
+
+    redirectToDay(requestDay);
+}
+
+// UTILS
+
+/**
+ * Adds a specified number of days to a given date.
+ *
+ * @param {Date} date - The date to which the days will be added.
+ * @param {number} days - The number of days to be added.
+ * @returns {string} - The resulting date in the format 'YYYY-MM-DD'.
+ */
+function addDays(date, days) {
+    const result = new Date(date);
+
+    result.setDate(result.getDate() + days);
+    return result.toLocaleDateString('en-CA');
 }
