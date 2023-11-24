9c0257c4e92d1d0d40961c6062ceec19b4a16001
Demmenty <Demmenty@gmail.com>
2023-11-25 00:00:06 +0400

    nutrition recommendations template refactor

---
+++ nutrition/templatetags/nutrition_recommendations.py
@@ -0,0 +1,26 @@
+from django.template.context import RequestContext
+from django import template
+from metrics.forms import NutritionRecsForm
+from metrics.models import NutritionRecs
+
+register = template.Library()
+
+# TODO replace nutricion recs to nutrition module
+
+
+@register.inclusion_tag("nutrition/recommendations.html", takes_context=True)
+def nutrition_recommendations(context: RequestContext) -> dict:
+    """Renders the nutrition recommendations form for a client"""
+
+    user = context.request.user
+    client = context.get("client") if user.is_expert else user
+
+    instance = NutritionRecs.objects.filter(client=client).first()
+    form = NutritionRecsForm(instance=instance)
+
+    data = {
+        "user": user,
+        "client": client,
+        "form": form,
+    }
+    return data
