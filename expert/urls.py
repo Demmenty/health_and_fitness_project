c3ac877826f1437ff1639962c40f69a5e3e5a996
Demmenty <Demmenty@gmail.com>
2023-12-26 18:27:01 +0400

    client info form views optimisation

--- expert/urls.py
+++ expert/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("", views.clients, name="clients"),
     path("archived/", views.archived_clients, name="archived_clients"),
     path("client/new/", views.client_new, name="client_new"),
-    path("client/<int:id>/profile", views.client_profile, name="client_profile"),
+    path("client/profile", views.client_profile, name="client_profile"),
     path("client/<int:id>/archive", views.client_archive, name="client_archive"),
     path("client/<int:id>/unarchive", views.client_unarchive, name="client_unarchive"),
     path(
@@ -21,7 +21,7 @@ urlpatterns = [
     path("client/questionnaires/food", views.client_food, name="client_food"),
     path("client/questionnaires/goal", views.client_goal, name="client_goal"),
     path("client/questionnaires/health", views.client_health, name="client_health"),
-    path("client/<int:id>/contacts/", views.client_contacts, name="client_contacts"),
+    path("client/contacts/", views.client_contacts, name="client_contacts"),
     path("note/main/save/", ajax.main_note_save, name="main_note_save"),
     path("note/monthly/get/", ajax.monthly_note_get, name="monthly_note_get"),
     path("note/monthly/save/", ajax.monthly_note_save, name="monthly_note_save"),
