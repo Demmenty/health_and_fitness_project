5ab015f41108a50f4511e911873dd13648e0d897
Demmenty <Demmenty@gmail.com>
2023-12-26 17:10:49 +0400

    client questionnaries added

--- client/admin.py
+++ client/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-from client.models import Contacts, Health, Log, Note
-
-admin.site.register(Health)
-admin.site.register(Contacts)
+from client.models import Contacts, Food, Goal, Health, Log, Note, Sleep, Weight
 
 
 @admin.register(Log)
@@ -15,7 +12,43 @@ class LogAdmin(admin.ModelAdmin):
     search_fields = ("modelname", "change_message")
 
 
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
+@admin.register(Health)
+class HealthAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
+@admin.register(Weight)
+class WeightAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
+@admin.register(Sleep)
+class SleepAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "client")
+    list_filter = ("client",)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
     list_display = ("__str__", "client")
     list_filter = ("client",)
